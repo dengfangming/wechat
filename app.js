1735c62f03b1acede2c89bb2e27eed634ad1106a
qaq Doe <johndoe@example.com>
2019-03-19 15:38:58 +0800

    连接服务器

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => {
+
+});
+
+app.listen(3000, err => {
+    if (!err) {
+        console.log('服务器启动成功');
+    } else {
+        console.log(err);
+    }
+});
